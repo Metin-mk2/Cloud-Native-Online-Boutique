1c383c240366cadd20111e6e1ee86aa9550e845e
Metin-mk2 <metin.karaguelle@gmail.com>
2025-12-15 20:55:01 +0100

    added gift wrapping service k8 and skaffold

--- src/giftwrappingservice/src/main/java/giftwrappingservice/GiftWrappingService.java
+++ src/giftwrappingservice/src/main/java/giftwrappingservice/GiftWrappingService.java
@@ -18,67 +18,113 @@ package giftwrappingservice;
 
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import io.grpc.health.v1.HealthCheckResponse.ServingStatus;
+import io.grpc.protobuf.services.HealthStatusManager;
 import io.grpc.stub.StreamObserver;
-
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
-import java.util.logging.Logger;
 
 public final class GiftWrappingService {
-    private static final Logger logger = Logger.getLogger(GiftWrappingService.class.getName());
+
+    private static final Logger logger = LogManager.getLogger(GiftWrappingService.class);
 
     private Server server;
+    private HealthStatusManager healthMgr;
 
     private static final GiftWrappingService service = new GiftWrappingService();
 
     private void start() throws IOException {
-        /* The port on which the server should run */
-        int port = 8080; // You might want to make this configurable
-        server = ServerBuilder.forPort(port)
-                .addService(new GiftWrappingServiceImpl())
-                .build()
-                .start();
-        logger.info("Server started, listening on " + port);
-        Runtime.getRuntime().addShutdownHook(new Thread() {
-            @Override
-            public void run() {
-                // Use stderr here since the logger may have been reset by its JVM shutdown
-                // hook.
-                System.err.println("*** shutting down gRPC server since JVM is shutting down");
-                try {
+    int port = Integer.parseInt(System.getenv().getOrDefault("PORT", "9600"));
+    healthMgr = new HealthStatusManager();
+
+    server =
+        ServerBuilder.forPort(port)
+            .addService(new GiftWrappingServiceImpl())
+            .addService(healthMgr.getHealthService())
+            .build()
+            .start();
+    logger.info("Gift Wrapping Service started, listening on " + port);
+    Runtime.getRuntime()
+        .addShutdownHook(
+            new Thread(
+                () -> {
+                  // Use stderr here since the logger may have been reset by its JVM shutdown hook.
+                  System.err.println(
+                      "*** shutting down gRPC GiftWrappingService server since JVM is shutting down");
                     GiftWrappingService.this.stop();
-                } catch (InterruptedException e) {
-                    e.printStackTrace(System.err);
-                }
-                System.err.println("*** server shut down");
-            }
-        });
+                  System.err.println("*** server shut down");
+                }));
+    healthMgr.setStatus("", ServingStatus.SERVING);
     }
 
-    private void stop() throws InterruptedException {
-        if (server != null) {
-            server.shutdown().awaitTermination(30, TimeUnit.SECONDS);
+    private void stop() {
+    if (server != null) {
+      healthMgr.clearStatus("");
+      server.shutdown();
         }
     }
 
-    /**
-     * Await termination on the main thread since the grpc library uses daemon
-     * threads.
-     */
+
+    private static GiftWrappingService getInstance() {
+        return service;
+    }
+
+    /** Await termination on the main thread since the grpc library uses daemon threads. */
     private void blockUntilShutdown() throws InterruptedException {
         if (server != null) {
             server.awaitTermination();
         }
     }
 
-    /**
-     * Main launches the server from the command line.
-     */
-    public static void main(String[] args) throws IOException, InterruptedException {
-        final GiftWrappingService giftWrappingService = new GiftWrappingService();
-        giftWrappingService.start();
-        giftWrappingService.blockUntilShutdown();
+
+
+    private static void initStats() {
+        if (System.getenv("DISABLE_STATS") != null) {
+            logger.info("Stats disabled.");
+            return;
+        }
+        logger.info("Stats enabled, but temporarily unavailable");
+
+        long sleepTime = 10; /* seconds */
+        int maxAttempts = 5;
+
+        // TODO(arbrown) Implement OpenTelemetry stats
+
     }
+
+    private static void initTracing() {
+        if (System.getenv("DISABLE_TRACING") != null) {
+            logger.info("Tracing disabled.");
+            return;
+        }
+        logger.info("Tracing enabled but temporarily unavailable");
+        logger.info("See https://github.com/GoogleCloudPlatform/microservices-demo/issues/422 for more info.");
+
+        // TODO(arbrown) Implement OpenTelemetry tracing
+
+        logger.info("Tracing enabled - Stackdriver exporter initialized.");
+    }
+
+    /** Main launches the server from the command line. */
+    public static void main(String[] args) throws IOException, InterruptedException {
+        new Thread(
+            () -> {
+                initStats();
+                initTracing();
+            })
+            .start();
+
+        // Start the RPC server. You shouldn't see any output from gRPC before this.
+        logger.info("GiftWrappingService starting.");
+        final GiftWrappingService service = GiftWrappingService.getInstance();
+        service.start();
+        service.blockUntilShutdown();
+    }
+
+
     //generate new proto with gradle build
     private static class GiftWrappingServiceImpl
             extends giftwrappingservice.GiftWrappingServiceGrpc.GiftWrappingServiceImplBase {
@@ -88,7 +134,11 @@ public final class GiftWrappingService {
               // TODO Auto-generated method stub
 
               //example response
-              Money giftwrapping = Money.newBuilder().setCurrencyCode("EUR").build();
+              //Sollte 5€ zurückgeben
+              Money giftwrapping = Money.newBuilder()
+                .setUnits(5)
+                .setNanos(100_000_000)
+                .setCurrencyCode("EUR").build();
               responseObserver.onNext(giftwrapping);
               }
     }
